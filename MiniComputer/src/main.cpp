c5d85f671f3409e002882fae468a795d24b8ec95
Blair Tate <btate7@my.bcit.ca>
2025-07-24 16:16:03 -0700

    Clear all automatically generated code

--- MiniComputer/src/main.cpp
+++ MiniComputer/src/main.cpp
@@ -1,20 +1,12 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  Serial.begin(9600); // initialize serial communication at 9600 baud rate
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+
 }
 
 void loop() {
-  Serial.println(myFunction(5, 7));
-  delay(1000); // wait for a second before repeating
+  
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+
