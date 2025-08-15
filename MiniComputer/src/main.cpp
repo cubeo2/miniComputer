365f8ca7bf8de17fc2606f965dc79ac8ceb2ec39
Blair Tate <btate7@my.bcit.ca>
2025-08-14 19:17:24 -0700

    Set up some structure for future code

--- MiniComputer/src/main.cpp
+++ MiniComputer/src/main.cpp
@@ -1,12 +1,50 @@
 #include <Arduino.h>
+#include "screen/screen.h"
+
+typedef uint8_t byte;
+
+byte state = 0;
 
 void setup() {
   Serial.begin(9600);
 
 }
 
-void loop() {
-  
+void updateScreen() {
+  // Update the screen with the current state
+  // should possibly calculate a couple a couple of frames, maybe a lot?
+  // Maybe another chip maintains the visual output until next time the method is called
+  test();
+  Serial.println("Updating screen...");
+} 
+void handleInput() {
+  // Handle input from the user
+  Serial.println("Handling input...");
 }
+void updateAudio() {
+  // maintain last sound state while other methods are called, using some buffer/chip
+  // Update audio output
+  Serial.println("Updating audio...");
+}
+void handleGameController() {
+  // Handle game controller input
+  Serial.println("Handling game controller...");
+}
+
+void loop() {
+
+  handleInput();
+  delay(1000);
+  handleGameController();
+    delay(1000);
+
+  updateAudio();
+    delay(1000);
+
+  updateScreen();
+    delay(1000);
+
+}
+
 
 
---
+++ MiniComputer/src/screen/screen.cpp
@@ -0,0 +1,7 @@
+#include <Arduino.h>
+#include "screen.h"
+
+void test() {
+    // This function is a placeholder for testing purposes
+    Serial.println("Test function called from screen module.");
+}
